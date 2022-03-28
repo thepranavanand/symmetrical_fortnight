21ff66e3d180edc79884fa9986bff2adbf78267a
thepranavanand <89196014+thepranavanand@users.noreply.github.com>
2022-03-28 12:36:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function () {
 
-var names = ["Pranav", "Jai", "Jaja", "Janav", "Addy", "Shaswat", "Divyam", "Anjil", "Akshat", "Jahnavi"];
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
